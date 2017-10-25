7bb0e27211e5a4376d5ee6cf045a808a90e888ee
Prasad Paranjape <=>
2017-10-26 02:10:32 +0530

    code review changes

--- server.js
+++ server.js
@@ -1,79 +1,30 @@
-var config = require('config');
+const config = require('config');
 const http = require('http');
+const request = require('request-promise');
 
 const hostname = '127.0.0.1';
 const port = config.port;
-
-function isEmptyObject(obj){
-    return JSON.stringify(obj) === '{}';
-}
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   var nodes = config.nodes;
   let node_name = config.util.getEnv('NODE_ENV');
-  req.setEncoding('utf8');
-  var response = [];
-  let rawData = ''; 
-  req.on('data', function (chunk) {
-     console.log("body: ", chunk);
-	 rawData += chunk;
-  });
-  
-  req.on('end', () => {
-	  let node_body = JSON.parse(rawData);
-
-	  nodes.forEach(function(item, index){
-		  if (!node_body.hasOwnProperty(node_name)) {
-
-		  let body = {};
-		  body[node_name] = nodes;
-
-		  if (!isEmptyObject(node_body)){
-			  for(var prop in node_body) {
-				  body[prop] = node_body[prop];
-			  }
-		  } 
-		  makeCall(item, body, function(results){
-			  response.push(results);
-			  res.end(JSON.stringify(response));
-		  });
-	  }
+	  const promises = nodes.map(url => request(url));
+	  let p = Promise.all(promises)
+	  .then(function(data){
+		return data;
+	  }, function(data){
+		return '?';
 	  });
-  });
+
+	  p.then(function(data){
+  	  	var a = {};
+  		a[node_name] = data;
+		res.end(JSON.stringify(a));
+	  });
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-
-
-function makeCall(port, body, callback) {
-	var data = JSON.stringify(body);
-	var options = {
-	  host: "127.0.0.1",
-	  port: port,
-	  path: '/',
-	  method: 'POST',
-	  headers: {
-		  'Content-Type': 'application/json',
-	  }
-	};
-
-	let req = http.request(options, function(res){
-  	  let rawData = '';
-  	  res.on('data', function (chunk) {
-  		rawData += chunk;
-  	  });
-	  res.setEncoding('utf8');
-	  
-	  callback(rawData);
-	});
-	req.on('error', (e) => {
-		callback({});
-	});
-	req.write(data);
-	req.end();
-}
